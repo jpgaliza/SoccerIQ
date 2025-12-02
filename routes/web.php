632e470cdeab35ebb2500a2eae18b0b177a3c1a3
Rafael-Bern <rafaelber@aluno.unifapce.edu.br>
2025-12-02 01:03:07 -0300

    Restaura rotas de perfil e auth para frontend

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return response()->json(['message' => 'Unauthenticated'], 401);
 })->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [\App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [\App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
